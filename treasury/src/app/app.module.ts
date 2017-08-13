c093d5fa505ab85a3d9f577aa0e392057dd06064
Alexander Herrmann <dArignac@users.noreply.github.com>
2017-08-13 15:59:07 +0200

    removed hammerjs

--- treasury/src/app/app.module.ts
+++ treasury/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
-import 'hammerjs';
-
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
