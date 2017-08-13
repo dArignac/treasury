cec59c37dbfc3a310fab1edc0e968888c3fd0183
Alexander Herrmann <dArignac@users.noreply.github.com>
2017-08-13 11:10:50 +0200

    initialized firebase app

--- treasury/src/app/app.module.ts
+++ treasury/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+
+import { environment } from '../environments/environment';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
