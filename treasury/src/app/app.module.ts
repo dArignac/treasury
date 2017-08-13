38f7a7bb9d310d11398bd70305e26d89e97d33b1
Alexander Herrmann <dArignac@users.noreply.github.com>
2017-08-13 13:03:55 +0200

    adjusted codestyle for imports

--- treasury/src/app/app.module.ts
+++ treasury/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from 'angularfire2';
 
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 
 
