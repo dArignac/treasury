e6524f82db5ae1e72cbaa8d57e2a641db85b20b6
Alexander Herrmann <dArignac@users.noreply.github.com>
2017-08-20 13:58:07 +0200

    some more for the service

--- treasury/src/app/services/catalog.service.ts
+++ treasury/src/app/services/catalog.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireDatabase } from "angularfire2/database";
+
+import { AuthService } from "./auth.service";
+import { UserService } from "./user.service";
+
+
 @Injectable()
 export class CatalogService {
 
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService, db: AngularFireDatabase) {
+  }
 
 }
