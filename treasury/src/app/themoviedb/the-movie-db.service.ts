a6043b5fddc9a30abd6e71a0284addb14c00ba29
Alexander Herrmann <darignac@gmail.com>
2017-09-13 10:02:35 +0200

    adjusted codestyle

--- treasury/src/app/themoviedb/the-movie-db.service.ts
+++ treasury/src/app/themoviedb/the-movie-db.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { environment } from "../../environments/environment";
-import { MovieResponse } from "./movie-response";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { MovieResponse } from './movie-response';
 
 @Injectable()
 export class TheMovieDbService {
