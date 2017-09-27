2cf3d2c6d5bc2cf0be8ec2029bc17bc61677c2ae
Alexander Herrmann <darignac@gmail.com>
2017-09-27 20:43:49 +0200

    rewrote and simplified the way on how to sync the data in firebase with the catalog data

--- treasury/src/app/services/catalog.service.ts
+++ treasury/src/app/services/catalog.service.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash';
+
 import { Injectable } from '@angular/core';
 
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
@@ -5,45 +7,88 @@ import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/data
 import 'rxjs/add/operator/first';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 import { AuthService } from './auth.service';
 import { MovieResponseItem } from '../themoviedb/movie-response-item';
 import { UserService } from './user.service';
 
 
+
 @Injectable()
 export class CatalogService {
+
+  private _catalogDB: Subscription;
 
   private _userCatalog: MovieResponseItem[] = [];
   private _userCatalogSubject: BehaviorSubject<MovieResponseItem[]> = new BehaviorSubject([]);
 
+  private _catalogIDs: number[] = []; // stores all ID of the elements in catalog (equivalent to the firebase catalog list of the user)
+  private _catalogItems: MovieResponseItem[] = [];
+
   constructor(private authService: AuthService, private userService: UserService, private db: AngularFireDatabase) {
-    db.list(`/users/${this.authService.id}/catalog`).subscribe(
+    this._catalogDB = db.list(`/users/${this.authService.id}/catalog`).subscribe(
       items => {
-        // FIXME how is this considering performance?
-        this._userCatalog = [];
-        for (let item of items) {
-          this.getItem(item.$key).first().subscribe(
-            itemDetail => {
-              this._userCatalog.push(itemDetail);
-              this._userCatalogSubject.next(
-                this._userCatalog.sort(
-                  (a, b) => {
-                    if (a.title < b.title) return -1;
-                    if (a.title > b.title) return 1;
-                    return 0;
-                  }
-                )
-              );
-            }
-          );
+        // FIXME if items are added, save their IDs. If they are removed, remove the ids - base verythin on that
+        // FIXME on addition/removal update a separate list with MovieResponseItems
+
+        // dehydrate the sent items collection to an array only containing the $key values
+        let firebaseIDs: number[] = _.flatMap(items, item => { return item.$key });
+
+        // FIXME remove consoles
+        console.log('firebaseIDs', firebaseIDs);
+        console.log('catalogIDs',  this._catalogIDs);
+
+
+        // calculate the difference between the IDs in firebase and our _catalogIDs
+        let added = _.difference(firebaseIDs, this._catalogIDs);
+        let removed = _.difference(this._catalogIDs, firebaseIDs);
+        // FIXME remove consoles
+        console.log('added', added);
+        console.log('removed', removed);
+
+        if (added.length > 0) {
+          this._catalogIDs = this._catalogIDs.concat(added);
         }
-      }
+        if (removed.length > 0) {
+          this._catalogIDs = _.pullAll(this._catalogIDs, removed);
+        }
+
+        // // FIXME how is this considering performance?
+        // this._userCatalog = [];
+        // for (let item of items) {
+        //   this.getItem(item.$key).first().subscribe(
+        //     itemDetail => {
+        //       this._userCatalog.push(itemDetail);
+        //       this._userCatalogSubject.next(
+        //         this._userCatalog.sort(
+        //           (a, b) => {
+        //             if (a.title < b.title) return -1;
+        //             if (a.title > b.title) return 1;
+        //             return 0;
+        //           }
+        //         )
+        //       );
+        //     }
+        //   );
+        // }
+        // FIXME remove consoles
+        console.log('--------------------------------------------------------------------------');
+        console.log('catalog is', this._catalogIDs);
+        console.log('###########################################################################');
+      },
+      // FIXME handle at least error
+      // e => {
+      //   console.log('error in fetching catalog', e);
+      // },
+      // () => {
+      //   console.log('COMPLETED');
+      // }
     );
   }
 
   get userCatalog(): Observable<MovieResponseItem[]> {
-    return this._userCatalogSubject.asObservable();
+    return this._userCatalogSubject.filter(v => !!v);
   }
 
   getItem(id: string): FirebaseObjectObservable<MovieResponseItem> {
