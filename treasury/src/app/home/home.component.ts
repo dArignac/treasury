edd474e6909bb9a4829d69801942b321f1f94da1
Alexander Herrmann <darignac@gmail.com>
2017-10-06 10:52:54 +0200

    first test with AngularFirestore

--- treasury/src/app/home/home.component.ts
+++ treasury/src/app/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { AuthService } from '../services/auth.service';
+
+import * as firebase from 'firebase/app';
+
+import { User } from '../services/user';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +15,32 @@ import { AuthService } from '../services/auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private afs: AngularFirestore) { }
+
+  private userCollection: AngularFirestoreCollection<User>;
+  users: Observable<User[]>;
+  private itemDoc: AngularFirestoreDocument<User>;
+  item: Observable<User>;
 
   ngOnInit() {
+    this.userCollection = this.afs.collection<User>('users');
+    this.users = this.userCollection.valueChanges();
+
+    this.itemDoc = this.afs.doc<User>('users/1');
+    this.item = this.itemDoc.valueChanges();
+  }
+
+  buttonClicked() {
+    console.log('button clicked');
+    let u = <User>{displayName: 'Ursula'};
+    this.itemDoc.update(u).then(() => {
+      console.log('user was updated');
+    });
+    // this.userCollection.add(this.authService.user).then(a => {
+    //   console.log('done');
+    //   console.log(a);
+    // });
+    console.log('button clicked, done');
   }
 
 }
---
+++ treasury/src/app/services/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+  displayName: string;
+  // email: string;
+  // isEmailVerified: boolean;
+  // photoURL: string;
+  // id: string;
+  // isCatalogPublic: boolean;
+}
