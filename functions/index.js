b1dd931cac44603c8fb6b86311fe6c0c253f2ce4
Alexander Herrmann <darignac@gmail.com>
2018-01-24 16:55:25 +0100

    playing around, this works #47

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,18 @@
-const functions = require('firebase-functions');
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+const functions = require('firebase-functions')
 
 exports.createMovie = functions.firestore
-  .document('users/*/movies/{movieId}')
+  .document('users/{userId}/movies/{movieId}')
   .onCreate(event => {
     // Get an object representing the document
     // e.g. {'name': 'Marie', 'age': 66}
-    var newValue = event.data.data();
+    //var newValue = event.data.data();
+
+    var userId = event.params.userId;
+    var movieId = event.params.movieId;
 
     // access a particular field as you would any JS property
-    var name = newValue.name;
+    //var name = newValue.name;
 
     // perform desired operations ...
+    console.log('user ' + userId + ' added the movie ' + movieId);
 });
