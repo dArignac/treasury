a16fcb2507b65b2dae23a4f30998a95c5a07f491
Alexander Herrmann <darignac@gmail.com>
2018-01-24 11:41:25 +0100

    added dummy function #47

--- treasury/functions/src/index.ts
+++ treasury/functions/src/index.ts
@@ -1,8 +1,6 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.countMovies = functions.firestore
+  .document('users/{userId}/movies').onCreate(event => {
+    console.log('creation test');
+  });
