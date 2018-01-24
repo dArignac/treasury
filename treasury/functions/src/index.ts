0555da237d0208b9acb707d00b2d19895d574b70
Alexander Herrmann <darignac@gmail.com>
2018-01-24 11:49:44 +0100

    changed to hello world function for now #47

--- treasury/functions/src/index.ts
+++ treasury/functions/src/index.ts
@@ -1,6 +1,12 @@
 import * as functions from 'firebase-functions';
 
-exports.countMovies = functions.firestore
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!\n\n');
+ });
+
+/*
+export const countMovies = functions.firestore
   .document('users/{userId}/movies').onCreate(event => {
     console.log('creation test');
   });
+*/
