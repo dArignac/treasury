aabf31f05a2f44e02f6db575369dbec360de5f10
Alexander Herrmann <darignac@gmail.com>
2019-12-17 18:24:19 +0100

    imported CommonModule to fix several warning

--- treasury/src/app/app.module.ts
+++ treasury/src/app/app.module.ts
@@ -1,35 +1,32 @@
-import * as Raven from 'raven-js';
-
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
-
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as Raven from 'raven-js';
 import { environment } from '../environments/environment';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthGuard } from './services/auth-guard.service';
-import { AuthService } from './services/auth.service';
+import { AppComponent } from './app.component';
 import { BaseComponent } from './base/base.component';
 import { ErrorPageDefaultComponent } from './error-page-default/error-page-default.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormAddComponent } from './form-add/form-add.component';
-import { ImprintComponent } from './imprint/imprint.component';
 import { HomeComponent } from './home/home.component';
+import { ImprintComponent } from './imprint/imprint.component';
 import { MaterialModule } from './material/material.module';
 import { MenuComponent } from './menu/menu.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SettingsComponent } from './settings/settings.component';
-import { TheMovieDbService } from './themoviedb/the-movie-db.service';
+import { AuthGuard } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
 import { BackupService } from './services/backup.service';
 import { UserService } from './services/user.service';
-
+import { SettingsComponent } from './settings/settings.component';
+import { TheMovieDbService } from './themoviedb/the-movie-db.service';
 
 if (environment.sentryDSN.length > 0) {
   Raven.config(environment.sentryDSN).install();
@@ -61,6 +58,7 @@ export class RavenErrorHandler implements ErrorHandler {
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
@@ -73,7 +71,7 @@ export class RavenErrorHandler implements ErrorHandler {
     TheMovieDbService,
     UserService,
     BackupService,
-    {provide: ErrorHandler, useClass: RavenErrorHandler}
+    { provide: ErrorHandler, useClass: RavenErrorHandler }
   ],
   bootstrap: [AppComponent]
 })
