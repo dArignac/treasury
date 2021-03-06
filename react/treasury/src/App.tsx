b986fd7062e6856beebaedf626726f5ac1955b51
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-03-06 19:05:49 +0100

    added app bar

--- react/treasury/src/App.tsx
+++ react/treasury/src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import './App.css';
+import Menu from './menu/Menu';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// FIXME colors suck, need something dark and pastel
+// current palette #525252 #414141 #313131 #CA3E47
+// need to check after more components have been added
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#525252'
+    },
+    secondary: {
+      main: '#ca3e47'
+    }
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Menu />
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
