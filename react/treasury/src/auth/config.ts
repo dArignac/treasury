4796223c974330f01f66833d649f12c4ed168b6a
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-03-07 15:28:31 +0100

    fixed codestyle #130

--- react/treasury/src/auth/config.ts
+++ react/treasury/src/auth/config.ts
@@ -1,19 +1,20 @@
 type TFirebaseConfig = {
-	apiKey: string,
-	projectId: string,
-	databaseURL: string,
-	authDomain: string,
-	storageBucket: string,
-	messagingSenderId: string
+	apiKey: string;
+	projectId: string;
+	databaseURL: string;
+	authDomain: string;
+	storageBucket: string;
+	messagingSenderId: string;
 };
 
 const firebaseConfig: TFirebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID as string,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL as string,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN as string,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID as string,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL as string,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN as string,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET as string,
+	messagingSenderId: process.env
+		.REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string,
 };
 
 export default firebaseConfig;
