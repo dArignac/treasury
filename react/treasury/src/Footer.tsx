d03f00dd6c087ad27b67809013cd391132dd7f66
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-03-13 07:03:32 +0100

    extended footer

--- react/treasury/src/Footer.tsx
+++ react/treasury/src/Footer.tsx
@@ -7,8 +7,14 @@ const useStyles = makeStyles((theme) => ({
     "& a": {
       color: theme.palette.common.white,
     },
+    display: "grid",
+    gap: "0 0",
+    gridTemplateColumns: "minmax(min-content, 400px)",
+    gridTemplateRows: "1fr",
     height: "100%",
-    padding: "1em",
+    justifyContent: "center",
+  },
+  inner: {
     textAlign: "center",
   },
 }));
@@ -17,9 +23,27 @@ export default function Footer() {
   const classes = useStyles();
   return (
     <footer className={classes.root}>
-      Treasury is an open source application. Find it at{" "}
-      <a href="https://github.com/darignac/treasury">Github</a>. Created 2017 -
-      2021 by <a href="https://github.com/darignac">darignac</a>.
+      <div className={classes.inner}>
+        <p>
+          Treasury is an open source application. Find it at{" "}
+          <a href="https://github.com/darignac/treasury">Github</a>. Created
+          2017 - 2021 by <a href="https://github.com/darignac">darignac</a>.
+        </p>
+        <p>Treasury proudly uses:</p>
+        <p>
+          <a href="https://www.themoviedb.org/">
+            <img
+              src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg"
+              alt="The Movie DB"
+            />
+          </a>
+        </p>
+        <p>
+          <a href="https://firebase.google.com/">
+            <img src="firebase.svg" alt="Built with Firebase" />
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
