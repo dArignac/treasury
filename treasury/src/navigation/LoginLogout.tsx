8cc166b82b739afd53809dbd597fad9865281b62
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-01-25 19:04:25 +0100

    switched login/logout to new firebase version

--- treasury/src/navigation/LoginLogout.tsx
+++ treasury/src/navigation/LoginLogout.tsx
@@ -1,26 +1,36 @@
 import Button from "@material-ui/core/Button";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import React from "react";
 import { FirebaseStore } from "../store";
 
 export default function LoginLogout() {
   const isSignedIn = FirebaseStore.useState((s) => s.isLoggedIn);
+  const auth = getAuth();
+  const login = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {})
+      .catch((error) => {});
+  };
+  const logout = () => {
+    signOut(auth)
+      .then((result) => {})
+      .catch((error) => {});
+  };
   return (
     <React.Fragment>
       {isSignedIn ? (
-        <Button color="inherit" onClick={() => firebase.auth().signOut()}>
+        <Button color="inherit" onClick={() => logout()}>
           Logout
         </Button>
       ) : (
         <React.Fragment>
-          <Button
-            color="inherit"
-            onClick={() => {
-              const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-              firebase.auth().signInWithPopup(googleAuthProvider);
-            }}
-          >
+          <Button color="inherit" onClick={() => login()}>
             Login
           </Button>
         </React.Fragment>
